582f64a6bd251c0442021ecd1d0d8a2cad3bdff6
Smahesh26 <sunainamahesh1@gmail.com>
2025-04-06 23:07:53 +0530

    db

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    webpack: (config, { isServer }) => {
+      if (!isServer) {
+        config.resolve.fallback = {
+          ...config.resolve.fallback,
+          canvas: false, // ignore canvas in browser
+          fs: false,
+          path: false,
+        };
+      }
+      return config;
+    },
+  };
+  
+  module.exports = nextConfig;
+  
