8166563dc39679783a65d6547fd139f48470e3f9
Smahesh26 <sunainamahesh1@gmail.com>
2025-10-08 11:02:10 +0530

    courses

--- next.config.js
+++ next.config.js
@@ -29,11 +29,11 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "img.freepik.com",
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '/dnycwq6ad/**',
       },
     ],
-    domains: ['i.ytimg.com'],
   },
 };
 
